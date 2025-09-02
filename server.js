b96941b4308e43a5c05d7bd5b5db1569008513cb
AngelRico12 <angelrico122001@gmail.com>
2025-09-01 20:03:52 -0600

    Prueba swagger

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+
+const app = express();
+const swaggerDocument = YAML.load("./swagger.yaml");
+
+// Ruta donde se mostrará la documentación
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}/docs`);
+});
