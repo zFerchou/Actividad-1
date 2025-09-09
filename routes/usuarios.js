e165fb910bba9f8c822b9931297e3441b20210dc
zFerchou <luisfernandoma94@gmail.com>
2025-09-09 07:40:03 -0600

    front

--- routes/usuarios.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { listarUsuarios, crearUsuario } = require('../controllers/UsuarioController');
-
-router.get('/', listarUsuarios);
-router.post('/nuevo', crearUsuario);
-
-module.exports = router;
